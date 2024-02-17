739aea8c468a68db725b27a84eba27982ac2d732
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-02-16 20:56:44 -0500

    src/cli.rs:reorder help output
    v0.0.3
    
    gnostr: git+nostr rust relay
    
    Usage: gnostr-relay [OPTIONS]
    
    Options:
      -p, --port <PORT>      Use the <port> as the listening port <u16>
      -d, --db <DB>          Use the <directory> as the location of the database
      -c, --config <CONFIG>  Use the <file name> as the location of the config file
      -h, --help             Print help
      -V, --version          Print version

--- src/cli.rs
+++ src/cli.rs
@@ -6,6 +6,13 @@ pub struct CLIArgs {
     #[arg(
         short,
         long,
+        help = "Use the <port> as the listening port <u16>",
+        required = false
+    )]
+    pub port: Option<u16>,
+    #[arg(
+        short,
+        long,
         help = "Use the <directory> as the location of the database",
         required = false
     )]
@@ -14,13 +21,6 @@ pub struct CLIArgs {
         short,
         long,
         help = "Use the <file name> as the location of the config file",
-        required = false
-    )]
-    pub port: Option<u16>,
-    #[arg(
-        short,
-        long,
-        help = "Use the <port> as the listening port <u16>",
         required = false
     )]
     pub config: Option<String>,
