ca8054488f77a06ca93003c95908891cd762fa3d
randymcmillan <randymcmillan@protonmail.com>
2024-07-18 13:05:47 -0400

    src/bin/gnostr-relay-process.rs

--- src/bin/gnostr-relay-process.rs
+++ src/bin/gnostr-relay-process.rs
@@ -1,11 +1,20 @@
+use std::process::{Command, ExitStatus};
+use std::io::Result;
+
+pub fn execute(exe: &str, args: &[&str]) -> Result<ExitStatus> {
+    Command::new(exe).args(args).spawn()?.wait()
+}
+
 async fn heavy_computation() {
     tokio::time::sleep(std::time::Duration::from_millis(2000)).await;
     println!("heavy computation finished");
+    execute("gnostr-relay",&["-p", "1111"]);
 }
 
 async fn light_computation() {
     tokio::time::sleep(std::time::Duration::from_millis(500)).await;
     println!("light computation finished");
+    execute("gnostr-relay",&["-p", "1112"]);
 }
 
 #[tokio::main]
